2040f531da123b82a18b7703f11f827d9f5ed541
이동현 <jamesldh08@gmail.com>
2025-10-30 00:09:22 +0900

    Update master.js

--- Frontend/src/master.js
+++ Frontend/src/master.js
@@ -2,7 +2,7 @@ import { mount } from 'svelte'
 import master from './routes/Master.svelte'
 
 const app = mount(master, {
-  target: document.getElementById('app'),  // ✅ 'app'으로 수정
+  target: document.getElementById('app'),
 })
 
 export default app
